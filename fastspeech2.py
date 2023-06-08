6ce775cb194d8c0807d496c11bf8861671698e3d
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-06-08 14:57:11 +0900

    Update fastspeech2.py

--- fastspeech2.py
+++ fastspeech2.py
@@ -26,18 +26,18 @@ class FastSpeech2(nn.Module):
         if self.use_postnet:
             self.postnet = PostNet()
 
-    def forward(self, src_seq, src_len, mel_len=None, d_target=None, p_target=None, e_target=None, max_src_len=None, max_mel_len=None, dur_pitch_energy_aug=None, f0_stat=None, energy_stat=None):
-
+    def forward(self, src_seq, src_len, mel_len=None, d_target=None, p_target=None, e_target=None, max_src_len=None, max_mel_len=None):
         src_mask = get_mask_from_lengths(src_len, max_src_len)
         mel_mask = get_mask_from_lengths(mel_len, max_mel_len) if mel_len is not None else None
         
         encoder_output = self.encoder(src_seq, src_mask)
+        
         if d_target is not None:
             variance_adaptor_output, d_prediction, p_prediction, e_prediction, _, _ = self.variance_adaptor(
                 encoder_output, src_mask, mel_mask, d_target, p_target, e_target, max_mel_len)
         else:
             variance_adaptor_output, d_prediction, p_prediction, e_prediction, mel_len, mel_mask = self.variance_adaptor(
-                    encoder_output, src_mask, mel_mask, d_target, p_target, e_target, max_mel_len, dur_pitch_energy_aug, f0_stat, energy_stat)
+                    encoder_output, src_mask, mel_mask, d_target, p_target, e_target, max_mel_len)
         
         decoder_output = self.decoder(variance_adaptor_output, mel_mask)
         mel_output = self.mel_linear(decoder_output)
